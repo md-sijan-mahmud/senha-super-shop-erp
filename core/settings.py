ca4837a87a6de15a205b94150708180fe286c0b7
Sijan Mahmud <sijanmahmud2007@gmail.com>
2026-08-31 22:08:51 +0600

    Add STATIC_ROOT configuration

--- core/settings.py
+++ core/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/6.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -131,3 +131,5 @@ MAILERS = {
 LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/accounts/login/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
